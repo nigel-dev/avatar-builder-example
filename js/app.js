fefbdd382e9ed57ee6fdeeb5020b65b4152dfad4
Nigel <nbazzeghin@gmail.com>
2015-07-10 12:53:28 -0500

    Fully working avatar POC

--- js/app.js
+++ js/app.js
@@ -6,13 +6,86 @@ $(document).ready(function() {
   $('#slider1').tinycarousel({
     bullets: true
   });
+    
+    
 });
+
+var renderer = PIXI.autoDetectRenderer(440,300);
+
+renderer.backgroundColor = 0xFFFFFF;
+   // document.body.appendChild(renderer.view);
+
+    $("#avatar").append(renderer.view);
+
+    
+    
+    
+    // create the root of the scene graph
+    var stage = new PIXI.Container();
+
+    var spineBoy = null;
+
+    // load spine data
+    PIXI.loader
+        .add('avatar', 'avatar/Avitar.json')
+        .load(onAssetsLoaded);
+
+
+    $("#test").click(function(){
+
+            spineBoy.state.addAnimationByName(1, 'Idle2', false);
+            //spineBoy.state.setAnimationByName(0, 'Idle', true);
+    });
+
+function onAssetsLoaded(loader, res)
+{
+    // create a spine boy
+    spineBoy = new PIXI.spine.Spine(res.avatar.spineData);
+
+    // set the position
+    spineBoy.position.x = renderer.width / 2;
+    spineBoy.position.y = renderer.height;
+
+    spineBoy.scale.set(0.9);
+    
+   
+
+    // set up the mixes!
+//    spineBoy.stateData.setMixByName('Idle', 'Idle2', 0.1);
+//    spineBoy.stateData.setMixByName('Idle2', 'Idle', 0.4);
+
+    // play animation
+    //spineBoy.state.setAnimationByName(0, 'Idle2', false);
+    spineBoy.state.addAnimationByName(0, 'Idle', true);
+
+
+    stage.addChild(spineBoy);
+    stage.setBackgroundColor('0xFFFFFF');
+    
+    
+
+//    stage.on('click', function ()
+//    {
+//        spineBoy.state.setAnimationByName(0, 'Jump', false);
+//        spineBoy.state.addAnimationByName(0, 'Idle', true, 0);
+//    });
+}
+
+requestAnimationFrame(animate);
+
+function animate()
+{
+    requestAnimationFrame(animate);
+    renderer.render(stage);
+}
+
 
 /*
  * Click Actions.
  */
 
-$("#hairstyles, #faces, #hats, #shirts, #torsos, #other").click(function(event) {
+//$("#hairstyles, #faces, #hats, #shirts, #torsos, #other").click(function(event) {
+$("#shirts").click(function(event) {
 
   // Store which parent item triggered the menu display.
   localStorage.setItem("selectedArea", event.target.id);
@@ -56,7 +129,12 @@ function toggleMenuDisplay() {
 }
 
 $('.overview li').click(function (event) {
-    alert($(this).index());
+    //alert($(this).index());
+    
+    var item = $(this).index();
+    
+    
+    spineBoy.state.addAnimationByName(1, 'Idle' + item, false);
 
     localStorage.setItem(localStorage.getItem("selectedArea"), $(this).index());
 
